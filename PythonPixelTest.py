cff412001729a68323ff583a61b1ed3575fcf97e
Arthur Lambert <39805498+ArthurLambert@users.noreply.github.com>
2020-12-11 11:23:35 +0100

    [UPD] video transformation as well

--- PythonPixelTest.py
+++ PythonPixelTest.py
@@ -6,7 +6,10 @@ import cv2
 
 def makeHologram(original, finalsize, innersquare, distance=0):
     side = min(finalsize)
-    print(side)
+    if (innersquare >= side):
+        hologram = None
+        return hologram
+#    print(side)
     #scale = ()
     scale = 1
     scaleR = 4
@@ -15,10 +18,20 @@ def makeHologram(original, finalsize, innersquare, distance=0):
 
     #width = int((scale*original.shape[1]))
     width = int(side / (1.0 + 2.0 * (float)(original.shape[0]) / original.shape[1]))
-    print(width)
     #height = int((scale*original.shape[0]))
     height = int(side / (2.0 + (float)(original.shape[1]) / original.shape[0]))
-    print(height)
+#    print(height)
+#    print(width)
+    offset = 0
+    if (2 * height + innersquare >= side):
+        print("square")
+        offset = int((width - (side - innersquare) / 2 * (original.shape[1] / original.shape[0])) / 2 - (side - innersquare) / 2 + height)
+        height = int((side - innersquare) / 2)
+        width = int((side - innersquare) / 2 * (original.shape[1] / original.shape[0]))
+#        print(height)
+#        print(width)
+#        print(offset)
+    
 
     image = cv2.resize(original, (width, height), interpolation = cv2.INTER_CUBIC)
     
@@ -36,23 +49,24 @@ def makeHologram(original, finalsize, innersquare, distance=0):
     #vert_x = (int)(up.shape[1])/2
     #hologram[0:(int)(up.shape[0]), (int)(center_x-vert_x+distance):(int)(center_x+vert_x+distance)] = up
     #hologram[(int)(hologram.shape[0]-down.shape[0]):(int)(hologram.shape[0]) , (int)(center_x-vert_x+distance):(int)(center_x+vert_x+distance)] = down
-    hologram[0:height, height:width+height] = up
-    hologram[side - height:side, side - height - width:side - height] = down
+    
+    hologram[0:height, height + offset:width+height + offset] = up
+    hologram[side - height:side, side - height - width - offset:side - height - offset] = down
     
     #hori_y = (int)(right.shape[0])/2
     #hori_x = (int)(right.shape[1])/2
     #hologram[ (int)(center_x-hori_x) : (int)(center_x-hori_x+right.shape[0]) , (int)(hologram.shape[1]-right.shape[1]+distance) : (int)(hologram.shape[1]+distance)] = right
     #hologram[ (int)(center_x-hori_x) : (int)(center_x-hori_x+left.shape[0]) , (int)(0+distance) : (int)(left.shape[1]+distance) ] = left
-    hologram[height:height + width, side - height:side] = right
-    hologram[side - height - width:side - height, 0:height] = left
+    
+    hologram[height + offset:height + width + offset, side - height:side] = right
+    hologram[side - height - width - offset:side - height - offset, 0:height] = left
 
 
     #cv2.imshow("up",up)
     #cv2.imshow("down",down)
     #cv2.imshow("left",left)
     #cv2.imshow("right",right)
-    cv2.imshow("hologram",hologram)
-    cv2.waitKey()
+    #cv2.imshow("hologram",hologram)
     return hologram
 
 def process_video(video):
@@ -65,24 +79,24 @@ def process_video(video):
     while(not ret):
         ret, frame = cap.read()
         if ret:
-            frame = cv2.resize(frame, (640, 640), interpolation = cv2.INTER_CUBIC)
-            holo = makeHologram(frame)
+            #frame = cv2.resize(frame, (640, 640), interpolation = cv2.INTER_CUBIC)
+            holo = makeHologram(frame, (640, 640), 0)
     out = cv2.VideoWriter('hologram.avi',fourcc, 30.0, (holo.shape[0],holo.shape[1]))
-    total_frames = cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     count = 0
     print ("Processing %d frames"%(total_frames))
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret:
-            frame = cv2.resize(frame, (640, 640), interpolation = cv2.INTER_CUBIC)
-            holo = makeHologram(frame)
+            #frame = cv2.resize(frame, (640, 640), interpolation = cv2.INTER_CUBIC)
+            holo = makeHologram(frame, (640, 640), 0)
             out.write(holo)
             count += 1
             print ("Total:%d of %d"%(count,total_frames))
         if(count>=total_frames-1):
             break
-    
+
     # Release everything if job is finished
     cap.release()
     out.release()
@@ -116,8 +130,8 @@ if __name__ == '__main__' :
     if (len(sys.argv) == 2) :
         #try :
         orig = cv2.imread(sys.argv[1])
-        holo = makeHologram(orig, (1920, 500), 100)
-            #process_video("/home/evan/Videos/test.avi")
+        holo = makeHologram(orig, (1920, 720), 400)
+        process_video("C:/Users/arthu/Pictures/clip_snowball.mp4")
             #cv2.imwrite("hologram.png",holo)
 #        except ValueError:
 #            exit(84)
